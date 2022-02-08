ee79608fb63eecdcaaad784d732872e34f693d9f
mainazarov1 <mainazarov1@gmail.com>
2022-02-08 18:14:48 +0600

    feat: add validation and generate module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LinkModule } from './link/link.module';
 
 @Module({
-  imports: [],
+  imports: [LinkModule],
   controllers: [AppController],
   providers: [AppService],
 })
